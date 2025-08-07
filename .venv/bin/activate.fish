021eaf50a18f3fabba02cf713adfaaca9d6117ce
Vinicius Araújo <vinicius.araujo@vinicius.araujo>
2025-08-07 19:08:07 -0300

    feat: concluindo dashboard com histograma e scatter plot

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/Users/vinicius.araujo/Desktop/Sprint5- Triple10/.venv'
+set -gx VIRTUAL_ENV /Users/vinicius.araujo/Desktop/Sprint5/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
